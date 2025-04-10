d35959b6924801d0ca9fa605250cd4de004645bf
Ivor Bosloper <ivorbosloper@gmail.com>
2025-04-11 01:50:52 +0200

    Make CropLegendControl work

--- map/crop/CropLegendControl.js
+++ map/crop/CropLegendControl.js
@@ -1,4 +1,29 @@
 import Control from 'ol/control/Control';
+import VectorTile from 'ol/layer/VectorTile';
+import {toGeometry} from "ol/render/Feature";
+import TileState from "ol/TileState";
+import {intersects} from "ol/extent";
+import debounce from "lodash.debounce";
+
+function forFeaturesInExtent(vt, extent, callback) {
+  const renderer = vt.getRenderer();
+  const tileCache = renderer.getTileCache();
+  if (tileCache.getCount() === 0) return;
+
+  const tileGrid = renderer.getLayer().getSource().tileGrid;
+  const z = tileGrid.getZForResolution(renderer.renderedResolution);
+  tileCache.forEach((tile) => {
+    if (tile.tileCoord[0] !== z || tile.getState() !== TileState.LOADED) return;
+    for (const sourceTile of tile.getSourceTiles()) {
+      const tileCoord = sourceTile.tileCoord;
+      if (intersects(extent, tileGrid.getTileCoordExtent(tileCoord))) {
+        for (const candidate of  sourceTile.getFeatures()) {
+          if (intersects(extent, candidate.getExtent())) callback(candidate);
+        }
+      }
+    }
+  });
+}
 
 /**
  * A custom Legend Control for OpenLayers
@@ -14,13 +39,77 @@ export class CropLegendControl extends Control {
       target: options.target,
     });
 
+    this.attribute = options.attribute;
+    this.mapping = options.mapping || {};
     this.legendItems = [];
     this.render();
+    this.tileLoadEnd = this.tileLoadEnd.bind(this);
+    this.changeSource = this.changeSource.bind(this);
+    this.updateFeatureCount = debounce(this.updateFeatureCount, 1000).bind(this);
+  }
+  updateFeatureCount(e) {
+    const map = this.getMap();
+    const vectortiles = map.getLayers().getArray().filter(f => f instanceof VectorTile);
+    const extent = map.getView().calculateExtentInternal();
+    let totalArea = 0, count = 0, cropArea = {}, cropCount = {};
+    for (const vt of vectortiles) {
+      forFeaturesInExtent(vt, extent, (feature) => {
+          const area = feature.properties_["area"] || toGeometry(feature).getArea();
+          const crop = feature.properties_[this.attribute];
+          count++;
+          totalArea += area;
+          cropCount[crop] = (cropCount[crop] || 0) + 1;
+          cropArea[crop] = (cropArea[crop] || 0) + area;
+      });
+    }
+    const topCrops = Object.entries(cropArea)
+      .map(([crop, area]) => ({ crop, area }))
+      .sort((a, b) => b.area - a.area) // Descending order
+      .slice(0, 5);
+
+    this.legendItems = topCrops.map(({crop, area}) =>
+      {
+        const c = this.mapping[crop];
+        const percent = (area / totalArea) * 100;
+        return {
+          label: c.name.replaceAll("_", " "),
+          color: c.color || "#99bbccaa",
+          percent: percent.toFixed(2) + "%",
+          area: area.toFixed(2),
+        }
+      }
+    )
+    this.render();
+  }
+
+  tileLoadEnd(e) {
+    const features = e.tile.getFeatures();
+    for (const feature of features) {
+      const p = feature.getProperties();
+      p.color = this.mapping[p[this.attribute]]?.color || "#99bbccaa";
+    }
+    this.updateFeatureCount(e)
+  }
+
+  changeSource(e) {
+    if (!(e.element instanceof VectorTile)) return;
+    if (e.type === "add") {
+      e.element.getSource().on('tileloadend', this.tileLoadEnd);
+    } else {
+      e.element.getSource().un('tileloadend', this.tileLoadEnd);
+    }
   }
 
   setMap(map) {
     super.setMap(map);
-
+    const layers = map.getLayers();
+    if (map) {
+      map.on("moveend", this.updateFeatureCount);
+      layers.on(["add", "remove"], this.changeSource)
+    } else {
+      map.un("moveend", this.updateFeatureCount);
+      layers.un(["add", "remove"], this.changeSource)
+    }
   }
   render() {
     const element = this.element;
--- map/crop/crop.js
+++ map/crop/crop.js
@@ -4,10 +4,6 @@ import {hcat} from "./codes";
 import VectorTile from "ol/layer/VectorTile";
 import {PMTilesVectorSource} from "ol-pmtiles";
 const cropExtension = "https://fiboa.github.io/hcat-extension/v0.1.0/schema.yaml";
-import debounce from "lodash.debounce";
-import TileState from 'ol/TileState';
-import {intersects} from "ol/extent";
-import {toGeometry} from "ol/render/Feature";
 import {CropLegendControl} from "./CropLegendControl";
 
 const CROP_ATTRIBUTE = "ec:hcat_code";
@@ -16,56 +12,15 @@ const fieldStyle = {
   "stroke-width": 0.5,
   "fill-color":  ['get', 'color']
 }
-const mapping = Object.fromEntries(hcat.map(c => [c.code, c.color]));
-
-function forFeaturesInExtent(vt, extent, callback) {
-  const renderer = vt.getRenderer();
-  const tileCache = renderer.getTileCache();
-  if (tileCache.getCount() === 0) return;
-
-  const tileGrid = renderer.getLayer().getSource().tileGrid;
-  const z = tileGrid.getZForResolution(renderer.renderedResolution);
-  tileCache.forEach((tile) => {
-    if (tile.tileCoord[0] !== z || tile.getState() !== TileState.LOADED) return;
-    for (const sourceTile of tile.getSourceTiles()) {
-      const tileCoord = sourceTile.tileCoord;
-      if (intersects(extent, tileGrid.getTileCoordExtent(tileCoord))) {
-        for (const candidate of  sourceTile.getFeatures()) {
-          if (intersects(extent, candidate.getExtent())) callback(candidate);
-        }
-      }
-    }
-  });
-}
+const mapping = Object.fromEntries(hcat.map(c => [c.code, c]));
 
 class CropMap extends FiboaMap {
   constructor() {
     super();
     this.fieldStyle = fieldStyle;
-    this.vectortiles = [];
-    this.updateFeatureCount = debounce(this.updateFeatureCount, 1000).bind(this);
-    this.map.on("moveend", this.updateFeatureCount)
-    this.map.addControl(new CropLegendControl());
+    this.map.addControl(new CropLegendControl({attribute: CROP_ATTRIBUTE, mapping}));
   }
-  updateFeatureCount(e) {
-      // const extent = this.map.getView().calculateExtent();
-      const extent = this.map.getView().calculateExtentInternal();
-      const result = {
-        area: 0,
-        count: 0,
-        perCrop: {}
-      }
-      for (const vt of this.vectortiles) {
-        forFeaturesInExtent(vt, extent, (feature) => {
-            const area = feature.properties_["area"] || toGeometry(feature).getArea();
-            const crop = feature.properties_[CROP_ATTRIBUTE];
-            result.count++;
-            result.area += area;
-            result[crop] = (result[crop] || 0) + area;
-        });
-      }
-      console.log(extent, result);
-  }
+
   addPMTilesLayer(c, options) {
     const source = new PMTilesVectorSource(options);
     const fields = new VectorTile({
@@ -78,15 +33,7 @@ class CropMap extends FiboaMap {
     });
     // TODO, Count the features in view and display a legend explaining the top 5 crops
     // TODO, Add a filter based on crops. This will not be perfect on high zoom levels (lossy vector tiles)
-    this.vectortiles.push(fields);
-    source.on('tileloadend', e => {
-      const features = e.tile.getFeatures();
-      for (const feature of features) {
-        const p = feature.getProperties();
-        p.color = mapping[p[CROP_ATTRIBUTE]] || "#99bbccaa";
-      }
-      this.updateFeatureCount(e)
-    })
+
     this.map.addLayer(fields);
   }
 }
